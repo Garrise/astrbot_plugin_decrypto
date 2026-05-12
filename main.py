1ba61a9c0c011ffa372fe1e8694b52bb371ce6cc
Garrise <dan19920316@gmail.com>
2026-05-12 10:08:44 +0800

    modified:   main.py

--- main.py
+++ main.py
@@ -41,11 +41,17 @@ class DecryptoSession():
         # 状态标识
         self.start_flag = False
         self.custom_keywords = False
+        self.custom_keywords_set = False
         self.keywords_sent = False
         self.is_game_set = False
         self.black_confirmed = False
         self.white_confirmed = False
         self.game_set_reply = ""
+
+        #计时
+        self.black_timeout = encrypt_timeout
+        self.white_timeout = encrypt_timeout
+        self.encrypt_start_time: int = 0
 
     def shuffle_teams(self):
         random.shuffle(self.black_teams)
@@ -259,9 +265,6 @@ class DecryptoPlugin(Star):
         self.encrypt_timeout = self.config.get("encrypt_timeout", 1800)  # 默认加密阶段总超时1800秒/30分钟
         self.timeout_tasks = {}
         self.remaining_time = {}
-        self.black_timeout = self.encrypt_timeout
-        self.white_timeout = self.encrypt_timeout
-        self.encrypt_start_time: int = 0
 
     async def _start_timeout_task(self, session_id: str):
         #检查先前的超时任务，如果有，取消它
@@ -272,6 +275,8 @@ class DecryptoPlugin(Star):
         #获取当前游戏进程
         if session_id in self.sessions:
             session: DecryptoSession = self.sessions[session_id]
+        else:
+            return
         async def time_reminder(remaining):
             while remaining > 0:
 
@@ -289,8 +294,8 @@ class DecryptoPlugin(Star):
             if session.phase == 0:
                 if session.turn % 2 == 0: #白方加密
                     # 剩余时间提醒
-                    await self.context.send_message(f"default:GroupMessage:{session_id}", MessageChain().message(f"剩余时间：{self.white_timeout // 60}分钟！"))
-                    remaining = self.white_timeout
+                    await self.context.send_message(f"default:GroupMessage:{session_id}", MessageChain().message(f"剩余时间：{session.white_timeout // 60}分钟！"))
+                    remaining = session.white_timeout
 
                     await time_reminder(remaining)
                     
@@ -311,8 +316,8 @@ class DecryptoPlugin(Star):
                     del self.sessions[session_id]
                 else: #黑方加密
                     # 剩余时间提醒
-                    await self.context.send_message(f"default:GroupMessage:{session_id}", MessageChain().message(f"剩余时间：{self.black_timeout // 60}分钟！"))
-                    remaining = self.black_timeout
+                    await self.context.send_message(f"default:GroupMessage:{session_id}", MessageChain().message(f"剩余时间：{session.black_timeout // 60}分钟！"))
+                    remaining = session.black_timeout
                     
                     await time_reminder(remaining)
 
@@ -348,6 +353,7 @@ class DecryptoPlugin(Star):
                     for player in session.black_teams:
                         message_chain.at(player[1], player[0])
                 await self.context.send_message(f"default:GroupMessage:{session_id}", message_chain)
+                await self._start_timeout_task(session_id)
             elif session.phase == 2: #我方译码阶段
                 await self.context.send_message(f"default:GroupMessage:{session_id}", MessageChain().message(f"剩余时间：{self.decrypt_timeout // 60}分钟！"))
                 remaining = self.decrypt_timeout
@@ -384,6 +390,7 @@ class DecryptoPlugin(Star):
                     reply = session.turn_change()
                     await self.context.send_message(f"default:GroupMessage:{session_id}", MessageChain(reply))
                     await self.context.send_message(f"default:FriendMessage:{session.encrypter}", MessageChain().message(f"你的密码是：{session.password}")) 
+                    await self._start_timeout_task(session_id)
         task = asyncio.create_task(timeout_handler())
         self.timeout_tasks[session_id] = task
         logger.info(f"Started timeout task for session {session_id}")
@@ -519,7 +526,7 @@ class DecryptoPlugin(Star):
             if mode.lower() == "custom" or mode.lower() == "自定义":
                 #等待法官私聊关键字
                 session.custom_keywords = True
-                yield event.plain_result("请法官私聊机器人发送关键字")
+                yield event.plain_result("请法官私聊机器人发送关键字。\n格式：\n/截码 自定义 [群号] 黑队关键词1,黑队关键词2,黑队关键词3,黑队关键词4|白队关键词1,白队关键词2,白队关键词3,白队关键词4")
                 return
             else:
                 #分发关键字
@@ -534,16 +541,16 @@ class DecryptoPlugin(Star):
     @decrypto.command("自定义", alias={"custom"})
     async def custom_keywords(self, event: AstrMessageEvent, group_id: str, keywords: str):
         session_id = group_id
+        if not event.is_private_chat():
+            yield event.plain_result("请私聊机器人发送关键字！")
+            event.stop_event()
+            return
         if session_id not in self.sessions:
             yield event.plain_result("截码战尚未开始！")
             event.stop_event()
             return
         async with self.group_locks[session_id]:
             session: DecryptoSession = self.sessions[session_id]
-            if not session.start_flag:
-                yield event.plain_result("游戏尚未开始！")
-                event.stop_event()
-                return
             if not session.custom_keywords:
                 yield event.plain_result("本局游戏不使用自定义关键字！")
                 event.stop_event()
@@ -558,17 +565,17 @@ class DecryptoPlugin(Star):
                 yield event.plain_result("玩家不能设置关键字！")
                 event.stop_event()
                 return
-            if session.black_keywords or session.white_keywords:
+            if session.custom_keywords_set:
                 yield event.plain_result("关键字已经设置！")
                 event.stop_event()
                 return
             else:
                 if "|" not in keywords:
-                    yield event.plain_result("关键字格式错误！\n格式：\n 黑队关键词(用半角逗号隔开)|白队关键词(用半角逗号隔开)")
+                    yield event.plain_result("关键字格式错误！\n格式：\n黑队关键词1,黑队关键词2,黑队关键词3,黑队关键词4|白队关键词1,白队关键词2,白队关键词3,白队关键词4")
                     event.stop_event()
                     return
                 if "," not in keywords.split("|")[0] or "," not in keywords.split("|")[1]:
-                    yield event.plain_result("关键字格式错误！\n格式：\n 黑队关键词(用半角逗号隔开)|白队关键词(用半角逗号隔开)")
+                    yield event.plain_result("关键字格式错误！\n格式：\n黑队关键词1,黑队关键词2,黑队关键词3,黑队关键词4|白队关键词1,白队关键词2,白队关键词3,白队关键词4")
                     event.stop_event()
                     return
                 black_keywords = keywords.split("|")[0].split(",")
@@ -591,6 +598,8 @@ class DecryptoPlugin(Star):
                 for member_id, _  in session.white_teams:
                     print(f"发送白队关键字给{member_id}")
                     await self.context.send_message(f"default:FriendMessage:{member_id}", MessageChain().message(f"关键字：{', '.join(session.white_keywords)}"))
+                session.custom_keywords_set = True
+                session.keywords_sent = True
                 yield event.plain_result("关键字已设置！")
 
     @decrypto.command("关键字", alias={"keywords"})
@@ -606,10 +615,6 @@ class DecryptoPlugin(Star):
             session: DecryptoSession = self.sessions[session_id]
             if session.start_flag:
                 yield event.plain_result("游戏已经开始！无法处理关键字！")
-                event.stop_event()
-                return
-            if session.custom_keywords:
-                yield event.plain_result("本局游戏使用自定义关键字，无法自动处理关键字！")
                 event.stop_event()
                 return
             if not session.keywords_sent:
@@ -634,7 +639,7 @@ class DecryptoPlugin(Star):
                     yield event.chain_result(reply)
                     await self.context.send_message(f"default:FriendMessage:{session.encrypter}", MessageChain().message(f"你的密码是：{session.password}"))
                     # 开始计时
-                    self.encrypt_start_time = int(time.perf_counter())
+                    session.encrypt_start_time = int(time.perf_counter())
                     await self._start_timeout_task(session_id)
             elif confirmed.lower() == "重抽" or confirmed.lower() == "reroll":
                 sender_id = event.get_sender_id()
@@ -642,18 +647,30 @@ class DecryptoPlugin(Star):
                     if session.black_confirmed:
                         yield event.plain_result("黑队关键字已确认，无法重抽！")
                         return
-                    session._generate_new_keywords("黑")
-                    for member in session.black_teams:
-                        await self.context.send_message(f"default:FriendMessage:{member[0]}", MessageChain().message(f"新的关键字：{', '.join(session.black_keywords)}"))
-                    yield event.plain_result("黑队关键字已重抽并发送，请查收私信！")
+                    if session.custom_keywords:
+                        session.custom_keywords_set = False
+                        session.keywords_sent = False
+                        yield event.plain_result("关键字已作废，请法官重新发送关键字！")
+                        return
+                    else:
+                        session._generate_new_keywords("黑")
+                        for member in session.black_teams:
+                            await self.context.send_message(f"default:FriendMessage:{member[0]}", MessageChain().message(f"新的关键字：{', '.join(session.black_keywords)}"))
+                        yield event.plain_result("黑队关键字已重抽并发送，请查收私信！")
                 elif any(member[0] == sender_id for member in session.white_teams):
                     if session.white_confirmed:
                         yield event.plain_result("白队关键字已确认，无法重抽！")
                         return
-                    session._generate_new_keywords("白")
-                    for member in session.white_teams:
-                        await self.context.send_message(f"default:FriendMessage:{member[0]}", MessageChain().message(f"新的关键字：{', '.join(session.white_keywords)}"))
-                    yield event.plain_result("白队关键字已重抽并发送，请查收私信！")
+                    if session.custom_keywords:
+                        session.custom_keywords_set = False
+                        session.keywords_sent = False
+                        yield event.plain_result("关键字已作废，请法官重新发送关键字！")
+                        return
+                    else:
+                        session._generate_new_keywords("白")
+                        for member in session.white_teams:
+                            await self.context.send_message(f"default:FriendMessage:{member[0]}", MessageChain().message(f"新的关键字：{', '.join(session.white_keywords)}"))
+                        yield event.plain_result("白队关键字已重抽并发送，请查收私信！")
                 else:
                     yield event.plain_result("你不在任何队伍中！")
             
@@ -687,11 +704,11 @@ class DecryptoPlugin(Star):
             yield event.chain_result(reply)
             # 停止计时
             encrypt_end_time = int(time.perf_counter())
-            elapsed_time = encrypt_end_time - self.encrypt_start_time
+            elapsed_time = encrypt_end_time - session.encrypt_start_time
             if session.turn % 2 == 0: #白方加密
-                self.white_timeout = self.white_timeout - elapsed_time
+                session.white_timeout = session.white_timeout - elapsed_time
             else: #黑方加密
-                self.black_timeout = self.black_timeout - elapsed_time
+                session.black_timeout = session.black_timeout - elapsed_time
             # 开始解密阶段超时任务
             await self._start_timeout_task(session_id)
 
@@ -741,13 +758,13 @@ class DecryptoPlugin(Star):
                     yield event.chain_result(reply)
                     await self.context.send_message(f"default:FriendMessage:{session.encrypter}", MessageChain().message(f"你的密码是：{session.password}"))
                     # 开始计时
-                    self.encrypt_start_time = int(time.perf_counter())
+                    session.encrypt_start_time = int(time.perf_counter())
                     await self._start_timeout_task(session_id)
                 else: 
                     yield event.plain_result(session.game_set_reply)
                     del self.sessions[session_id]
                     del self.group_locks[session_id]
-                    task = self.timeout_tasks[session]
+                    task = self.timeout_tasks[session_id]
                     if not task.done():
                         task.cancel()
                     del self.timeout_tasks[session_id]
@@ -801,6 +818,9 @@ class DecryptoPlugin(Star):
 
 "/截码 开始" 或 "/dc start"
   开始截码战游戏，分配关键字。
+
+"/截码 开始 自定义" 或 "/dc start custom"
+  开始截码战游戏，使用自定义关键字。随后请非玩家私聊机器人设置关键字！
 
 "/截码 关键字 [重抽/确认]" 或 "/dc keywords [reroll/confirm]"
   关键字确认或重抽指令，只有在游戏开始前可以使用。
